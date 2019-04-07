769801299da9a4f7c71bb3b6552e407943c673de
Michael L Perry <michael@qedcode.com>
2019-04-06 20:55:14 -0500

    Updated jinaga-react.

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import { Domain } from "@shared/model/visit";
-import { VisitCounter } from "./visit-counter";
 import { User } from "@shared/model/user";
+import { Domain } from "@shared/model/visit";
+import * as React from "react";
+import { VisitCounter } from "./visit-counter";
 
 export const App = ({ user }: { user: User }) => (
     <>
         <p>Welcome!</p>
-        <VisitCounter user={user} domain={ new Domain('myapplication') } />
+        <VisitCounter user={user} fact={ new Domain('myapplication') } />
     </>
 );
--- src/client/components/visit-counter.tsx
+++ src/client/components/visit-counter.tsx
@@ -1,37 +1,28 @@
-import { collection, field, useJinaga } from "jinaga-react";
-import * as React from "react";
 import { User } from "@shared/model/user";
 import { Domain, Visit } from "@shared/model/visit";
+import { array, field, jinagaContainer, specificationFor } from "jinaga-react";
+import * as React from "react";
 import { j } from "../jinaga-config";
 
-export interface VisitCounterProps {
-    user: User;
-    domain: Domain;
-}
+const visitCounterSpec = specificationFor(Domain, {
+    domain: field(d => d),
+    visits: array(j.for(Visit.inDomain), {
+        hash: field(v => j.hash(v))
+    })
+});
 
-interface VisitViewModel {
-    key: string;
-}
-
-interface VisitCounterState {
-    visits: VisitViewModel[];
-}
-
-export const VisitCounter = ({ user, domain }: VisitCounterProps) => {
-    const state = useJinaga<Domain, VisitCounterState>(j, domain, [
-        collection('visits', j.for(Visit.inDomain), v => v.key, [
-            field('key', v => j.hash(v))
-        ])
-    ]);
-
+const visitCounterMapping = visitCounterSpec<{
+    user: User
+}>(({ domain, visits, user }) => {
     React.useEffect(() => {
         // Record this user's visit.
-        j.fact(new Visit(domain, user))
+        j.fact(new Visit(domain, user, new Date()))
             .catch(err => console.error(err));
     }, []);
 
-    return (state.visits.length > 0
-        ? <p>You are visitor number {state.visits.length}.</p>
-        : null
+    return (
+        <p>You are visitor number {visits.length}.</p>
     );
-};
+});
+
+export const VisitCounter = jinagaContainer(j, visitCounterMapping);
