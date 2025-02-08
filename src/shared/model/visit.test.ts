34c683ec2bb4833fca0c2a238648ebeef911bede
Michael L Perry <michael@qedcode.com>
2025-02-08 09:03:45 -0600

    Fix tests

--- src/shared/model/visit.test.ts
+++ src/shared/model/visit.test.ts
@@ -1,6 +1,7 @@
 import { Jinaga, JinagaTest } from "jinaga";
 import { User } from "./user";
 import { Domain, Visit } from "./visit";
+import { model } from "./model";
 
 var j: Jinaga;
 
@@ -10,9 +11,13 @@ beforeEach(() => {
     });
 });
 
+const visitsInDomain = model.given(Domain).match(domain =>
+    domain.successors(Visit, visit => visit.domain).select(visit => Jinaga.hash(visit))
+);
+
 test("Visit count is initially zero", async () => {
     const domain = new Domain('myapplication');
-    const visits = await j.query(domain, j.for(Visit.inDomain));
+    const visits = await j.query(visitsInDomain, domain);
     expect(visits.length).toBe(0);
 });
 
@@ -20,6 +25,6 @@ test("Visit count increments to 1", async () => {
     const { userFact: user } = await j.login<User>();
     const domain = new Domain('myapplication');
     await j.fact(new Visit(domain, user, new Date()));
-    const visits = await j.query(domain, j.for(Visit.inDomain));
+    const visits = await j.query(visitsInDomain, domain);
     expect(visits.length).toBe(1);
 });
